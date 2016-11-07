dd4d4f78664d730ef1c8c424bcf4105243a9099d
lombardo-chcg <dpa.lombardo@gmail.com>
2016-11-07 06:55:50 -0600

    add solution 12

--- javascript/12.js
+++ javascript/12.js
@@ -8,29 +8,75 @@ const E = require('./Euler-Utils.js');
 // count factors of the sum
 
 
-function solution(numDivisors) {
-	let naturalNumsArray = [1];
-	let triangleNumberArray = []
-	let divisorCount = triangleNumberArray.length;
+function numsFrom(input) {
+  let number = input;
 
-	while (divisorCount < numDivisors) {
-
-		let currentTriangleNum = E.lastIn(triangleNumberArray);
-		let factors = E.factorsOf(currentTriangleNum)
-		factors.push(currentTriangleNum)
-		divisorCount = factors.length;
-		
-		console.log(divisorCount)
-
-		if (divisorCount < numDivisors) {
-			triangleNumberArray.push(E.sumArray(naturalNumsArray))
-			naturalNumsArray.push(E.lastIn(naturalNumsArray) + 1)
-		}
-
-	}
-
-	return E.lastIn(triangleNumberArray);
+  return function() {
+    return number += 1;
+  }
 }
 
-const answer = solution(45);
+function makeNthTriangleNumber(nth) {
+	let iterator = numsFrom(0);
+	let output = 0;
+	let counter = 0
+
+	while (counter < nth) {
+		output += iterator();
+		counter ++;
+	}
+
+	return output;
+}
+
+function getFactors(num) {
+	let factors = [];
+	let iterator = numsFrom(0);
+
+	let cur = iterator();
+
+	while (cur <= num/2) {
+		if (E.isFactor(cur, num)) {
+			factors.push(cur)
+		}
+
+		cur = iterator();
+	}
+
+	factors.push(num);
+
+	return factors;
+}
+
+
+function solution(numDivisors) {
+		let getNextNaturalNumber = numsFrom(0);
+		let triangleNumber = 0;
+		let factorCount = 0;
+
+		while (factorCount < numDivisors ) {
+			triangleNumber += getNextNaturalNumber();
+			factorCount = getFactors(triangleNumber).length;
+		}
+
+		return {
+			factorCount: factorCount,
+			triangleNumber: triangleNumber
+		}
+}
+
+// Start the timer
+const start = new Date().getTime();
+
+const answer = solution(500);
 console.log(answer);
+//=> { factorCount: 576, triangleNumber: 76576500 }
+
+
+// Stop the timer
+const end = new Date().getTime();
+const elapsed = (end - start) * .001;
+
+console.log('Execution time: ' + elapsed + ' seconds');
+
+//=> Execution time: 3684.09 seconds  // 61.4015 minutes
