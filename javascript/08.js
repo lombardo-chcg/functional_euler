47d11f4a112acb89e864e5bff42da4099bcb2d37
Dominick Lombardo <dpa.lombardo@gmail.com>
2016-11-20 06:16:47 +0000

    functional refactor

--- javascript/08.js
+++ javascript/08.js
@@ -1,30 +1,38 @@
 'use strict';
 
-// import my homemade 'Euler Utils' library
-const E = require('./Euler-Utils.js');
-
 // The four adjacent digits in the 1000-digit number that have the greatest product are 9 × 9 × 8 × 9 = 5832.
 const series = '7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450';
 // Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?
 
-function adjacentDigitsSum(series, adjacentDigitsLength) {
-  const seriesArray = series.split('');
-  const length = seriesArray.length;
-  let index = 0;
-  let largestProduct = 0;
+function sumSubArray(array, startingIndex, sliceLength) {
+  if (array[startingIndex+sliceLength] !== undefined) {
 
-  while (index < length - (adjacentDigitsLength-1)) {
-    let trashArray = seriesArray.map( (number) => number);
-    let subArray = trashArray.splice(index, adjacentDigitsLength);
-    let product = E.multiplyArray(subArray);
-
-    if (product > largestProduct) { largestProduct = product; }
-
-    index += 1;
+    return array
+      .slice(startingIndex, (startingIndex + sliceLength))
+      .reduce(( acc, cur ) => acc * cur );   
   }
-
-  return largestProduct;
+  
+  return 0;
 }
+
+function adjacentDigitsSum(series, adjacentDigitsLength) {
+  
+  return series
+    .split('')
+    .map(string => Number(string))
+    .map((number, index, array) => sumSubArray(array, index, adjacentDigitsLength))
+    .reduce((acc, cur) => acc > cur? acc : cur)
+}
+
+const start = new Date().getTime();
 
 const answer = adjacentDigitsSum(series, 13);
 console.log(answer);
+// => 23514624000
+
+// Stop the timer
+const end = new Date().getTime();
+const elapsed = (end - start) * .001;
+
+console.log('Execution time: ' + elapsed + ' seconds');
+//=> Execution time: 0.018000000000000002 seconds
