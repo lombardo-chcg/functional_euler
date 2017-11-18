9c17c847d49e9bb436ec811d6e1b0228e712e2a2
Dominick Lombardo <dpa.lombardo@gmail.com>
2017-11-18 14:50:56 -0600

    Add files via upload

---
+++ src/main/scala/com/lombardo/app/problems/Euler42.scala
@@ -0,0 +1,22 @@
+package com.lombardo.app.problems
+
+import scala.io.Source
+
+class Euler42 {
+  def triangleNums(max: Int): Stream[Int] = Stream.from(1).take(max).map(n => ((0.5 * n) * (n + 1)).toInt)
+  def triNumsUnder(max: Int): Stream[Int] = {
+    Stream.from(1).map(n => ((0.5 * n) * (n + 1)).toInt).takeWhile(_ < max)
+  }
+  def getNumericalValue(s: String): Int = {
+    s.toCharArray.map(_.toInt - 64).sum
+  }
+  def readFile(file: String): Iterator[String] = {
+    Source.fromInputStream(getClass.getResourceAsStream(file)).getLines
+  }
+  def solve:Int = {
+    val listOfWords = readFile("/p042_words.txt").mkString.split(",").map(_.replaceAll("\"", "")).toList
+    val maxWordValue = getNumericalValue(listOfWords.maxBy(_.length).split("").map(_ => "Z").mkString)
+    val triNumMap = triNumsUnder(maxWordValue + 1).foldLeft(Map.empty[Int, Boolean])((acc, cur) => acc + (cur -> true))
+    listOfWords.map(getNumericalValue).count(triNumMap.contains)
+  }
+}
---
+++ src/test/scala/com/lombardo/app/Euler42Test.scala
@@ -0,0 +1,23 @@
+package com.lombardo.app
+
+import com.lombardo.app.helpers.PrimeCalculator
+import com.lombardo.app.helpers.Utils.time
+import com.lombardo.app.problems._
+import org.scalatest.{FlatSpec, Matchers}
+
+class Euler42Test extends FlatSpec with Matchers {
+  implicit val pc = new PrimeCalculator
+  val e = new Euler42
+
+  it should "have good helpers" in {
+    e.triangleNums(10).toList should be(List(1, 3, 6, 10, 15, 21, 28, 36, 45, 55))
+    e.triNumsUnder(56) should be(List(1, 3, 6, 10, 15, 21, 28, 36, 45, 55))
+    e.getNumericalValue("SKY") should be(55)
+  }
+
+  it should "solve" in {
+    time(s"${getClass.getSimpleName.replace("Test", "")}") {
+      e.solve should be(162)
+    }
+  }
+}
