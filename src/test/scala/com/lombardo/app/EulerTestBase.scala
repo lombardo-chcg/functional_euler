405f488de3acd643cad2d440068c8dc9d3a88d12
lombardo-chcg <dpa.lombardo@gmail.com>
2018-03-24 18:45:53 -0500

    just trying anything at this point, tests running locally just time

--- src/test/scala/com/lombardo/app/EulerTestBase.scala
+++ src/test/scala/com/lombardo/app/EulerTestBase.scala
@@ -3,8 +3,5 @@ package com.lombardo.app
 import org.scalatest.{BeforeAndAfterAll, FlatSpec, Matchers}
 
 trait EulerTestBase extends FlatSpec with Matchers with BeforeAndAfterAll {
-  override def afterAll = {
-    println("after " + this.getClass.getSimpleName)
-    System.gc()
-  }
+  override def afterAll = System.gc()
 }
