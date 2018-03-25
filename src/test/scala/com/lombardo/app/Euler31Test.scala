b639093d5420ab799f3ff2704583f2ab2d2299fc
lombardo-chcg <dpa.lombardo@gmail.com>
2018-03-25 14:45:35 -0500

    solve 31

--- src/test/scala/com/lombardo/app/Euler31Test.scala
+++ src/test/scala/com/lombardo/app/Euler31Test.scala
@@ -3,14 +3,12 @@ package com.lombardo.app
 import com.lombardo.app.helpers.Utils.time
 import com.lombardo.app.problems._
 
-class zzzTestTemplate extends EulerTestBase {
-  val e = new Euler01
-
-  it should "have good helpers" in {
-  }
+class Euler31Test extends EulerTestBase {
+  val e = new Euler31
 
   it should "solve" in {
     time(s"${getClass.getSimpleName.replace("Test", "")}") {
+      e.solve should be(73682)
     }
   }
 }
